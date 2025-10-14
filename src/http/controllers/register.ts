1fbeb1f2b38678893df88a7bf65d7a58029617e7
navestelar <acch.escola@gmail.com>
2025-10-13 21:41:46 -0300

    feat: global error handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -32,5 +32,5 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     return reply.status(201).send() // TODO: fix me
   }
 
-  return reply.status(201).send()
+  throw err
 }
