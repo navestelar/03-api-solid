f0f602848f05878609d2d4c32140da2b2856f075
navestelar <acch.escola@gmail.com>
2026-01-13 21:20:50 -0300

    chore: test environments

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,4 +3,25 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        expand: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+      {
+        expand: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
+  },
 })
